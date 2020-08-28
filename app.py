33d9ed3c48b646d68971418ecac7acea7d24b1c6
bluewink <59544716+bluewink@users.noreply.github.com>
2020-08-28 14:37:09 +0900

    add class and code refactoring

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def home():
 def dashboard():
     return render_template('test.html')
 
-
+'''
 def find_dict_mismatch(one_line , dict_list):   #국어사전
     cnt = 0
     for one_word in one_line:
@@ -73,24 +73,8 @@ def find_dict_match(one_line , dict_list):  #특수문자 사전
             cnt += 1
             print(one_word)
     return cnt
+    '''
 
-'''
-def find_dict_match(one_line, dict_list):
-    cnt = 0
-    f = open("dict.txt", 'rt', encoding='UTF8') 
-    special_dict = []
-    while True:
-        error_word = f.readline()
-        error_word = error_word.strip()
-
-        if not error_word: break
-        special_dict.append(error_word)
-        
-
-        cnt = cnt + one_line.count(error_word)
-        #print(error_word)
-    f.close()
-    return cnt
 '''
 def get_article_contents(news_link):
     contents=[]
@@ -101,7 +85,7 @@ def get_article_contents(news_link):
         if type(content) is bs4.element.NavigableString and len(content)>50:
              contents.append(content.strip())
     return contents
-
+'''
 
 def preprocess_string(string_list):
     noun_list = []
@@ -113,81 +97,10 @@ def preprocess_string(string_list):
     return noun_list
 
 '''
-
-@app.route('/post', methods=['POST'])
-def post():
-    value = request.form['test']
-    data=[]
-    section_ids = ["100", "101", "102"]
-    for section_id in section_ids:
-        url = value
-        url = url.replace(str(100), "%s") % section_id
-        res = requests.get(url)
-        soup = bs(res.text, "lxml")
-        lis=soup.find_all('li', class_= "ranking_item", limit=3)
-        for li in lis:
-            news_link = "https://news.naver.com" + li.a.attrs.get('href')
-            #####[뉴스 내용 가져오기 함수 추가]
-            s = get_article_contents(news_link)
-            data.append(s)
-
-#이중 리스트 하나로 만들기
-    data=list(chain.from_iterable(data))
-
-#엑셀 생성
-    wb = Workbook()
-    ws = wb.create_sheet("뉴스",0)
-    row=2
-    ws.cell(1,1,"뉴스")
-
-#엑셀에 입력
-    for i in data:
-        ws.cell(row,1,i)
-        row+=1
-    wb.save('output.xlsx')
-
-    wb = load_workbook('output.xlsx', read_only = True)
-    data1 = wb.active
-    script=[]
-    for row in data1.iter_rows():
-        for cell in row:
-            var1=cell.value
-            script.append(var1)
-
-    line_cnt = 0
-    error_cnt = 0
-
-    noun_list = preprocess_string(script)
-    
-    #-------
-    
     excel_data_df = pd.read_excel('korean_dict.xlsx', sheet_name='NIADic')
     korean_dict_list = excel_data_df['term'].tolist()
-    
-
-    f = open("dict.txt", 'rt', encoding='UTF8') 
-    special_dict = []
-    while True:
-        error_word = f.readline()
-        error_word = error_word.strip()
-
-        if not error_word: break
-        special_dict.append(error_word)
-    #-------
-    
-
-    for one_line in noun_list:
-        line_cnt += 1
-        error_cnt += find_dict_mismatch(one_line, korean_dict_list)
-        error_cnt += find_dict_match(one_line, special_dict)
-
-    error_pcnt = str(error_cnt / len(noun_list) * 100)
-    #print(error_cnt)
-    #print(line_cnt)
-    return "품질 오류 : " + error_pcnt + "% 입니다."
-
 '''
-#@app.route('/user/signup', methods=['POST'])
+
 
 @app.route('/post', methods=['POST'])
 def crawl():
